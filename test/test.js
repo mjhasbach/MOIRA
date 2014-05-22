eff78fe23823fb20a908197d119961b12e2d2740
Matthew Hasbach <hasbach.git@gmail.com>
2014-05-21 22:18:27 -0400

    Change error string to error object.

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var moira = require( '../lib/moira' );
 
 moira.getIP( function( err, ip, service ){
-    if( err ) throw err;
+    if ( err ) throw err;
 
     console.log( 'Your external IP address is ' + ip );
     console.log( 'The fastest service to return your IP address was ' + service );
